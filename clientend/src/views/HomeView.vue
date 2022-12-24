2500e0be313982352f29a39d8efd6778dba4a16e
Victor David <36716074+viccy2@users.noreply.github.com>
2022-12-24 21:20:35 +0100

    Updated

--- clientend/src/views/HomeView.vue
+++ clientend/src/views/HomeView.vue
@@ -487,105 +487,8 @@
           
       </v-container>
     </section>
-
-     <section id="team">
-      <v-container>
-          <div class="hidden-sm-and-down">
-            <h2 id="h25" >Meet the <span style="color:#673AB7"> Team </span></h2><br><br><br><br><br>
-            <div>
-            <v-layout row wrap>
-              <v-flex md3 lg3>
-                <center>
-                  <v-card flat class=" ma-1 animate__animated animate__fadeIn" >
-                    <v-img src="../assets/images/po.jpg" style="width:90%;height:200px;"></v-img>
-                  </v-card><br>
-                  <h2 id="h26" style="font-size:14px;color:#673AB7">Victor Aremu David </h2>
-                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Founder, Team Lead Engineering)  </h2>
-                </center>
-              </v-flex>
-              <v-flex md3 lg3>
-                <center>
-                  <v-card flat class=" ma-1 animate__animated animate__fadeIn" >
-                    <v-img src="../assets/images/23.jpeg" style="width:90%;height:200px;"></v-img>
-                  </v-card><br>
-                  <h2 id="h26" style="font-size:14px;color:#673AB7">Olaoluwa David</h2>
-                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Co-Founder, Chief Financial Officer)  </h2>
-                </center>             
-              </v-flex>
-              <v-flex md3 lg3>
-                <center>
-                  <v-card flat class=" ma-1 animate__animated animate__fadeIn" >
-                    <v-img src="../assets/images/afolabisalawu.jpg" style="width:90%;height:200px;"></v-img>
-                  </v-card><br>
-                  <h2 id="h26" style="font-size:14px;color:#673AB7">Salawu Afolabi </h2>
-                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Team Lead Development & Engineering (Backend))  </h2>
-                </center>
-              </v-flex>
-             
-              <v-flex md3 lg3>
-                <center>
-                  <v-card flat class=" ma-1 animate__animated animate__fadeIn" >
-                    <v-img src="../assets/images/n12.jpg" style="width:90%;height:200px;"></v-img>
-                  </v-card><br>
-                  <h2 id="h26" style="font-size:14px;color:#673AB7">Dada Lekan </h2>
-                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Team Lead UI / UX)  </h2>
-                </center>             
-              </v-flex>
-            </v-layout><br><br><br>
-          </div><br><br><br>
-          </div>
-          
-        <div class="hidden-md-and-up" id="con4">
-          <h2 id="h26">Meet the<span style="color:#673AB7"> Team.</span></h2><br><br>
-            <div>
-            
-              <center>
-            <v-layout row wrap>
-              <v-flex sm6 xs6>
-               
-                  <v-card flat class=" ma-2 animate__animated animate__fadeIn" >
-                    <v-img class="" src="../assets/images/po.jpg" style="width:100%;height:200px;"></v-img>
-                  </v-card><br>
-                  <h2 id="h26" style="font-size:14px;color:#673AB7">Victor Aremu David </h2>
-                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Founder, Team Lead Engineering)  </h2>
-                
-                <br><br><br>
-              </v-flex>
-              <v-flex sm6 xs6>
-                <center>
-                  <v-card flat class=" ma-2 animate__animated animate__fadeIn" >
-                    <v-img class="" src="../assets/images/23.jpeg" style="width:100%;height:200px;"></v-img>
-                  </v-card><br>
-                  <h2 id="h26" style="font-size:14px;color:#673AB7">Olaoluwa David</h2>
-                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Co-Founder, Chief Financial Officer)  </h2>
-                </center>
-                <br><br><br>
-              </v-flex>
-              <v-flex sm6 xs6>
-                <v-card flat class=" ma-2 animate__animated animate__fadeIn" >
-                    <v-img class="" src="../assets/images/afolabisalawu.jpg" style="width:100%;height:200px;"></v-img>
-                  </v-card><br>
-                  <h2 id="h26" style="font-size:14px;color:#673AB7">Salawu Afolabi </h2>
-                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Team Lead Development & Engineering (Backend))  </h2>
-                <br><br><br>              
-              </v-flex>
-              <v-flex sm6 xs6>
-                <v-card flat class=" ma-2 animate__animated animate__fadeIn" >
-                    <v-img class="" src="../assets/images/n12.jpg" style="width:100%;height:200px;"></v-img>
-                  </v-card><br>
-                  <h2 id="h26" style="font-size:14px;color:#673AB7">Dada Lekan </h2>
-                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Team Lead UI / UX )  </h2>
-                <br><br><br>                 
-              </v-flex>  
-            </v-layout><br><br><br><br>
-              </center>
-           
-            </div>
-        </div>
-          
-      </v-container>
-    </section>
-
+     
+    
     <section>
       <v-container>
           <div class="hidden-sm-and-down">
@@ -714,6 +617,105 @@
           </div>
       </v-container>
     </section>
+
+     <section id="team">
+      <v-container>
+          <div class="hidden-sm-and-down">
+            <h2 id="h25" >Meet the <span style="color:#673AB7"> Team </span></h2><br><br><br><br><br>
+            <div>
+            <v-layout row wrap>
+              <v-flex md3 lg3>
+                <center>
+                  <v-card flat class=" ma-1 animate__animated animate__fadeIn" >
+                    <v-img src="../assets/images/po.jpg" style="width:90%;height:200px;"></v-img>
+                  </v-card>
+                  <h2 id="h26" style="font-size:14px;color:#673AB7">Victor Aremu David </h2><br>
+                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Founder, Team Lead Engineering)  </h2>
+                </center>
+              </v-flex>
+              <v-flex md3 lg3>
+                <center>
+                  <v-card flat class=" ma-1 animate__animated animate__fadeIn" >
+                    <v-img src="../assets/images/23.jpeg" style="width:90%;height:200px;"></v-img>
+                  </v-card><br>
+                  <h2 id="h26" style="font-size:14px;color:#673AB7">Olaoluwa David</h2>
+                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Co-Founder, Chief Financial Officer)  </h2>
+                </center>             
+              </v-flex>
+              <v-flex md3 lg3>
+                <center>
+                  <v-card flat class=" ma-1 animate__animated animate__fadeIn" >
+                    <v-img src="../assets/images/afolabisalawu.jpg" style="width:90%;height:200px;"></v-img>
+                  </v-card><br>
+                  <h2 id="h26" style="font-size:14px;color:#673AB7">Salawu Afolabi </h2>
+                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Team Lead Development & Engineering (Backend))  </h2>
+                </center>
+              </v-flex>
+             
+              <v-flex md3 lg3>
+                <center>
+                  <v-card flat class=" ma-1 animate__animated animate__fadeIn" >
+                    <v-img src="../assets/images/n12.jpg" style="width:90%;height:200px;"></v-img>
+                  </v-card><br>
+                  <h2 id="h26" style="font-size:14px;color:#673AB7">Dada Lekan </h2>
+                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Team Lead UI / UX)  </h2>
+                </center>             
+              </v-flex>
+            </v-layout><br><br><br>
+          </div><br><br><br>
+          </div>
+          
+        <div class="hidden-md-and-up" id="con4">
+          <h2 id="h26">Meet the<span style="color:#673AB7"> Team.</span></h2><br><br>
+            <div>
+            
+              <center>
+            <v-layout row wrap>
+              <v-flex sm6 xs6>
+               
+                  <v-card flat class=" ma-2 animate__animated animate__fadeIn" >
+                    <v-img class="" src="../assets/images/po.jpg" style="width:100%;height:200px;"></v-img>
+                  </v-card>
+                  <h2 id="h26" style="font-size:14px;color:#673AB7">Victor Aremu David </h2><br>
+                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Founder, Team Lead Engineering)  </h2>
+                
+                <br><br><br>
+              </v-flex>
+              <v-flex sm6 xs6>
+                <center>
+                  <v-card flat class=" ma-2 animate__animated animate__fadeIn" >
+                    <v-img class="" src="../assets/images/23.jpeg" style="width:100%;height:200px;"></v-img>
+                  </v-card>
+                  <h2 id="h26" style="font-size:14px;color:#673AB7">Olaoluwa David</h2><br>
+                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Co-Founder, Chief Financial Officer)  </h2>
+                </center>
+                <br><br><br>
+              </v-flex>
+              <v-flex sm6 xs6>
+                <v-card flat class=" ma-2 animate__animated animate__fadeIn" >
+                    <v-img class="" src="../assets/images/afolabisalawu.jpg" style="width:100%;height:200px;"></v-img>
+                  </v-card>
+                  <h2 id="h26" style="font-size:14px;color:#673AB7">Salawu Afolabi </h2><br>
+                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Team Lead Development & Engineering (Backend))  </h2>
+                <br><br><br>              
+              </v-flex>
+              <v-flex sm6 xs6>
+                <v-card flat class=" ma-2 animate__animated animate__fadeIn" >
+                    <v-img class="" src="../assets/images/n12.jpg" style="width:100%;height:200px;"></v-img>
+                  </v-card>
+                  <h2 id="h26" style="font-size:14px;color:#673AB7">Dada Lekan </h2><br>
+                  <h2 id="h26" class="font-italic" style="font-size:12px;"> (Team Lead UI / UX )  </h2>
+                <br><br><br>                 
+              </v-flex>  
+            </v-layout><br><br><br><br>
+              </center>
+           
+            </div>
+        </div>
+          
+      </v-container>
+    </section>
+
 
     <section>
       <v-container>
